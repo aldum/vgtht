92acca314aaf5935c91dacb5727b686ce11b9e84
aldum <aldum@artixlinux.org>
2023-03-09 22:09:57 +0100

    traverse input directory

--- src/main/scala/pw/aldum/vgtht/Process.scala
+++ src/main/scala/pw/aldum/vgtht/Process.scala
@@ -1,4 +1,22 @@
 package pw.aldum.vgtht
 
+import java.nio.file.{ Files, Path }
+
+import scala.io.Source
+import scala.jdk.CollectionConverters.*
+
 object Process:
-  def apply(args: Args) = ???
+  def readInput(path: Path) =
+    val paths = Files.list(path).nn.iterator.nn
+    paths
+      .asScala
+      .filter(_.endsWith(".csv"))
+      .map { p =>
+        val lines = Source
+          .fromFile(p.toFile.nn)
+          .getLines
+        ParsedData(lines)
+      }
+
+  def apply(args: Args) =
+    readInput(args.in)
